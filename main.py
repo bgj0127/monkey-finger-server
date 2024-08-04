bcbb7bc0e1422dddfbba676f910612a503682ef3
miridih-e2etest <bgj0127@gmail.com>
2024-08-04 21:39:23 +0900

    [Add] openai 요청 작업 추가

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from typing import List
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from DB_Control import connDB, filtered_data
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
 engine, table = connDB("MONKEY_FINGER")
-
 app = FastAPI()
 
 origins = ["http://localhost:5173/", "https://monkeyfinger.netlify.app",
            "https://master--monkeyfinger.netlify.app"]
+
+
+# origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -41,39 +46,73 @@ def search_filter(filter_type: FilterType):
             return {"data": {"None"}, "status": e.status_code}
     # return {"language": filter_type.language, "mode": filter_type.mode}
 
-# @app.post("/uploadfile")
-# def upload_file(file: UploadFile):
-#     df = pd.read_csv(file.file)
-#     file.file.close()
-#     if "wpm" not in df.columns:
-#         return {"res": "Monkey Type csv 파일만 넣어주세요"}
-#     df.loc[df["isPb"].isna(), "isPb"] = 0
-#     df.loc[df["isPb"] == True, "isPb"] = 1
-#     for i in range(df.shape[0]):
-#         insert_data = f"""
-#         merge into monkey_type
-#         using dual
-#         on (id='{df.iloc[i]['_id']}')
-#         when not matched then
-#             insert
-#             (
-#                 id, is_pb, wpm, acc, test_mode, test_mode2, test_duration, language, timestamp
-#             )
-#             values(
-#             '{df.iloc[i]['_id']}',
-#             {df.iloc[i]['isPb']},
-#             {df.iloc[i]['wpm']},
-#             {df.iloc[i]['acc']},
-#             '{df.iloc[i]['mode']}',
-#             '{df.iloc[i]['mode2']}',
-#             {df.iloc[i]['testDuration']},
-#             '{df.iloc[i]['language']}',
-#             {df.iloc[i]['timestamp']}
-#         )
-#         """
-#         try:
-#             cur.execute(insert_data)
-#             cur.execute("commit")
-#         except cx_Oracle.DatabaseError as e:
-#             print(e)
-#     return {"res": file.filename}
+
+class TypingData(BaseModel):
+    wpm: float
+    acc: float
+
+
+client = OpenAI(
+    organization='org-MTh1QeshOBBE0hOZAi5z9Mfo',
+    project='proj_CH1M2LPgCzpMvmS2W4TJD4Fj',
+)
+
+client.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.post("/advice")
+def advice(typing: TypingData):
+    model = "gpt-3.5-turbo"
+    query = f"wpm {typing.wpm}, acc {typing.acc}"
+    messages = [{
+        "role": "system",
+        "content": '넌 원숭이고 타자연습 결과를 분석해서 문자열을 객체 형태로 반환.key는 영어, value는 한국어 친절한 말투로.{"wpm":,"acc":,"eval": {"speed":,"acc":},"recommend": { "maintain": ,"improve":,"encourage":}}'
+    }, {
+        "role": "user",
+        "content": query
+    }]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=1.3,
+        max_tokens=512,
+        frequency_penalty=1.0
+    )
+
+    return response.choices[0].message.content
+    # @app.post("/uploadfile")
+    # def upload_file(file: UploadFile):
+    #     df = pd.read_csv(file.file)
+    #     file.file.close()
+    #     if "wpm" not in df.columns:
+    #         return {"res": "Monkey Type csv 파일만 넣어주세요"}
+    #     df.loc[df["isPb"].isna(), "isPb"] = 0
+    #     df.loc[df["isPb"] == True, "isPb"] = 1
+    #     for i in range(df.shape[0]):
+    #         insert_data = f"""
+    #         merge into monkey_type
+    #         using dual
+    #         on (id='{df.iloc[i]['_id']}')
+    #         when not matched then
+    #             insert
+    #             (
+    #                 id, is_pb, wpm, acc, test_mode, test_mode2, test_duration, language, timestamp
+    #             )
+    #             values(
+    #             '{df.iloc[i]['_id']}',
+    #             {df.iloc[i]['isPb']},
+    #             {df.iloc[i]['wpm']},
+    #             {df.iloc[i]['acc']},
+    #             '{df.iloc[i]['mode']}',
+    #             '{df.iloc[i]['mode2']}',
+    #             {df.iloc[i]['testDuration']},
+    #             '{df.iloc[i]['language']}',
+    #             {df.iloc[i]['timestamp']}
+    #         )
+    #         """
+    #         try:
+    #             cur.execute(insert_data)
+    #             cur.execute("commit")
+    #         except cx_Oracle.DatabaseError as e:
+    #             print(e)
+    #     return {"res": file.filename}
