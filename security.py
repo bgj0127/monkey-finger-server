d6a4a9ac8d315002f013f468d549f74e44626ef4
JunB <51225580+bgj0127@users.noreply.github.com>
2024-08-07 19:29:12 +0900

    [Fix] 재수정

--- security.py
+++ security.py
@@ -13,7 +13,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
