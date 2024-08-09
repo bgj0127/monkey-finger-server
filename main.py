bd993aba61fc683252cbac3f32c69f402fda8378
JunB <51225580+bgj0127@users.noreply.github.com>
2024-08-09 12:30:15 +0900

    [Fix] cookie 도메인 설정

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app.include_router(user_router.app, tags=["user"])
 
 origins = [
     "https://monkeyfinger.netlify.app",
+    "monkeyfinger.netlify.app",
     "https://master--monkeyfinger.netlify.app",
 ]
 # origins = ["*"]
